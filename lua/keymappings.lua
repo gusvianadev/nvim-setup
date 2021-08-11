48feb4a7f7a7bca733c96563136dc41fd5c45881
gusvianadev <gusvianadev@gmail.com>
2021-08-11 12:09:52 -0300

    first commit

---
+++ lua/keymappings.lua
@@ -0,0 +1,48 @@
+-- Local variables
+local keymap = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+
+-- Map leader to space
+keymap('n', '<Space>', '<NOP>', opt)
+vim.g.mapleader = " "
+
+-- Press ii to return to normal mode in any mode
+keymap('i', 'ii', '<ESC>', opt)
+keymap('v', 'ii', '<ESC>', opt)
+keymap('c', 'ii', '<ESC>', opt)
+
+-- Visual mode
+keymap('v', '<TAB>', '>gv', opt)
+keymap('v', '<S-TAB>', '<gv', opt)
+keymap('v', 'K', ':move \'<-2<CR>gv-gv\'', opt)
+keymap('v', 'J', ':move \'>+1<CR>gv-gv\'', opt)
+
+-- Terminal mode
+keymap('n', '<LEADER>sh', ':terminal<CR>', opt)
+keymap('t', '<ESC>', '<C-\\><C-n>:bd!<CR>', opt)
+
+-- File management
+keymap('n', '<LEADER>sf', ':w<CR>', opt)
+keymap('n', '<LEADER>bd', ':bd<CR>', opt)
+
+-- Buffer navigation
+keymap('n', '<TAB>', ':bnext<CR>', opt)
+keymap('n', '<S-TAB>', ':bprevious<CR>', opt)
+
+-- Window navigation
+keymap('n', '<C-h>', '<C-w>h', opt)
+keymap('n', '<C-j>', '<C-w>j', opt)
+keymap('n', '<C-k>', '<C-w>k', opt)
+keymap('n', '<C-l>', '<C-w>l', opt)
+
+-- Tree Toggle
+keymap('n', '<LEADER>tt', ':NvimTreeToggle<CR>', opt)
+
+-- Use alt + hjkl to resize windows
+keymap('n', '<M-j>', ':resize -2<CR>', opt)
+keymap('n', '<M-k>', ':resize +2<CR>', opt)
+keymap('n', '<M-h>', ':vertical resize -2<CR>', opt)
+keymap('n', '<M-l>', ':vertical resize +2<CR>', opt)
+
+-- <TAB> for autocomplete
+keymap('i', '<expr><TAB>', 'pumvisible() ? \'\\<C-n>\' : \'\\<TAB>\'', opt)
---
+++ lua/plugins.lua
@@ -0,0 +1,22 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+    -- Packer itself
+    use 'wbthomason/packer.nvim'
+    -- Color scheme
+    use 'glepnir/oceanic-material'
+    -- Tree
+    use 'kyazdani42/nvim-web-devicons' 
+    use 'kyazdani42/nvim-tree.lua'
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-compe'
+end)
